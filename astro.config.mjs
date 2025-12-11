3289a23a76fcbcc572ffa53692d499c462bb84cc
Santiago Torres Aragon <contact@santiagotorres.me>
2025-12-11 14:36:23 -0500

    New layout and component for contacting me

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://santiagotorres.me/",
+  vite: { plugins: [tailwindcss()] },
 });
